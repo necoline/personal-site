0045d5f1614a362990f0de9596f4399f6e7169b8
necoline <hnhubner@gmail.com>
2017-09-27 19:36:39 -0700

    xml request for blog  working

--- js/main.js
+++ js/main.js
@@ -2,9 +2,31 @@ function loadProjects() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('demo').innerHTML = this.responseText;
+      document.getElementById('index').innerHTML = this.responseText;
     }
   };
   xhttp.open('GET', 'projects.html', true);
   xhttp.send();
 }
+
+function loadBlog() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('index').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'blogpage.html', true);
+  xhttp.send();
+}
+
+// function loadAbout() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById('index').innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open('GET', 'index.html', true);
+//   xhttp.send();
+// }
