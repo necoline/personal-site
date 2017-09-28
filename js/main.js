732c1e4c16e4833f4a5163dd85fb85639f20ed16
necoline <hnhubner@gmail.com>
2017-09-27 20:14:48 -0700

    internal routing complete

--- js/main.js
+++ js/main.js
@@ -20,13 +20,13 @@ function loadBlog() {
   xhttp.send();
 }
 
-// function loadAbout() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       document.getElementById('index').innerHTML = this.responseText;
-//     }
-//   };
-//   xhttp.open('GET', 'index.html', true);
-//   xhttp.send();
-// }
+function loadAbout() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('index').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'about.html', true);
+  xhttp.send();
+}
